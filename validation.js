a18a7c833cae6f2cd76b5b42f9695edcf6137c70
Anton Kivader <anton.kivader@plus4u.net>
2021-03-10 19:24:48 +0100

    Added meta tag to site be less responsive.

--- validation.js
+++ validation.js
@@ -1,6 +1,6 @@
 function writeColor() {
     var image = document.forms["formular"]["imgquestion"].value;
-    if (image === 4){
+    if (image === "4"){
         alert("Cant be 4");
         return false;
     }
