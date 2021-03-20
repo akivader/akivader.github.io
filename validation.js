aaa530927ad0b6deaa6088b37bc5ed2df52e8845
Anton Kivader <anton.kivader@plus4u.net>
2021-03-20 12:28:16 +0100

    Timing measured in seconds

--- validation.js
+++ validation.js
@@ -3,12 +3,12 @@ var lastClick;
 function writeColor() {
     var image = document.forms["formular"]["imgquestion"].value;
     var d = new Date();
-    var timeElapsed = d.getTime() - lastClick;
+    var timeElapsed = (d.getTime() - lastClick) /1000;
     if (image === "4"){
         alert("Cant be 4");
         return false;
     } else {
-        alert(timeElapsed);
+        alert("Form done in: " + timeElapsed +"s");
         return false;
     }
 }
