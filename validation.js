ffa86f7cccd28dce9377f97b8e30397f0fbdf265
Anton Kivader <anton.kivader@plus4u.net>
2021-03-20 01:57:18 +0100

    starting timer in JS

--- validation.js
+++ validation.js
@@ -1,7 +1,19 @@
+var lastClick;
+
 function writeColor() {
     var image = document.forms["formular"]["imgquestion"].value;
+    var d = new Date();
+    var timeElapsed = d.getTime() - lastClick;
     if (image === "4"){
         alert("Cant be 4");
         return false;
+    } else {
+        alert(timeElapsed);
+        return false;
     }
+}
+
+function timer(){
+    var d = new Date();
+    lastClick = d.getTime();
 }
