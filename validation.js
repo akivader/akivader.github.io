7f6ef36e122010a361afe4dfc0b73be294836ec0
Anton Kivader <anton.kivader@plus4u.net>
2021-03-27 13:33:48 +0100

    Fixed IF

--- validation.js
+++ validation.js
@@ -22,9 +22,6 @@ function toggleEnable(id) {
     if (textbox.disabled) {
         // If disabled, do this
         document.getElementById(id).disabled = false;
-    } else {
-        // Enter code here
-        document.getElementById(id).disabled = true;
     }
 }
 
